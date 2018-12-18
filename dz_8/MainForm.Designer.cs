2b951a4a09b2018fc02170088dafcf527d23e252
Dmitry1303 <45265651+Dmitry1303@users.noreply.github.com>
2018-12-18 16:51:00 +0300

    Add files via upload

---
+++ dz_8/MainForm.Designer.cs
@@ -0,0 +1,212 @@
+﻿namespace dz_8
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.файлToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.открытьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.сохранитьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.удалитьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.AddButton = new System.Windows.Forms.Button();
+            this.DellButton = new System.Windows.Forms.Button();
+            this.SaveButton = new System.Windows.Forms.Button();
+            this.QuestionBox = new System.Windows.Forms.TextBox();
+            this.NumberOfQuestion = new System.Windows.Forms.NumericUpDown();
+            this.TrueBox = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.NumberOfQuestion)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.Color.White;
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.файлToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(708, 33);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // файлToolStripMenuItem
+            // 
+            this.файлToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.открытьToolStripMenuItem,
+            this.сохранитьToolStripMenuItem,
+            this.удалитьToolStripMenuItem});
+            this.файлToolStripMenuItem.Name = "файлToolStripMenuItem";
+            this.файлToolStripMenuItem.Size = new System.Drawing.Size(65, 29);
+            this.файлToolStripMenuItem.Text = "Файл";
+            // 
+            // открытьToolStripMenuItem
+            // 
+            this.открытьToolStripMenuItem.Name = "открытьToolStripMenuItem";
+            this.открытьToolStripMenuItem.Size = new System.Drawing.Size(252, 30);
+            this.открытьToolStripMenuItem.Text = "Открыть";
+            this.открытьToolStripMenuItem.Click += new System.EventHandler(this.открытьToolStripMenuItem_Click);
+            // 
+            // сохранитьToolStripMenuItem
+            // 
+            this.сохранитьToolStripMenuItem.Name = "сохранитьToolStripMenuItem";
+            this.сохранитьToolStripMenuItem.Size = new System.Drawing.Size(252, 30);
+            this.сохранитьToolStripMenuItem.Text = "Сохранить";
+            this.сохранитьToolStripMenuItem.Click += new System.EventHandler(this.сохранитьToolStripMenuItem_Click);
+            // 
+            // удалитьToolStripMenuItem
+            // 
+            this.удалитьToolStripMenuItem.Name = "удалитьToolStripMenuItem";
+            this.удалитьToolStripMenuItem.Size = new System.Drawing.Size(252, 30);
+            this.удалитьToolStripMenuItem.Text = "Создать";
+            this.удалитьToolStripMenuItem.Click += new System.EventHandler(this.СоздатьToolStripMenuItem_Click);
+            // 
+            // AddButton
+            // 
+            this.AddButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.AddButton.Location = new System.Drawing.Point(13, 403);
+            this.AddButton.Name = "AddButton";
+            this.AddButton.Size = new System.Drawing.Size(116, 35);
+            this.AddButton.TabIndex = 1;
+            this.AddButton.Text = "Добавить";
+            this.AddButton.UseVisualStyleBackColor = true;
+            this.AddButton.Click += new System.EventHandler(this.AddButton_Click);
+            // 
+            // DellButton
+            // 
+            this.DellButton.BackColor = System.Drawing.Color.Gray;
+            this.DellButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.DellButton.Location = new System.Drawing.Point(237, 403);
+            this.DellButton.Name = "DellButton";
+            this.DellButton.Size = new System.Drawing.Size(116, 35);
+            this.DellButton.TabIndex = 1;
+            this.DellButton.Text = "Удалить";
+            this.DellButton.UseVisualStyleBackColor = false;
+            this.DellButton.Click += new System.EventHandler(this.DellButton_Click);
+            // 
+            // SaveButton
+            // 
+            this.SaveButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.SaveButton.Location = new System.Drawing.Point(125, 403);
+            this.SaveButton.Name = "SaveButton";
+            this.SaveButton.Size = new System.Drawing.Size(116, 35);
+            this.SaveButton.TabIndex = 1;
+            this.SaveButton.Text = "Сохранить";
+            this.SaveButton.UseVisualStyleBackColor = true;
+            this.SaveButton.Click += new System.EventHandler(this.SaveButton_Click);
+            // 
+            // QuestionBox
+            // 
+            this.QuestionBox.Location = new System.Drawing.Point(13, 37);
+            this.QuestionBox.Multiline = true;
+            this.QuestionBox.Name = "QuestionBox";
+            this.QuestionBox.Size = new System.Drawing.Size(683, 360);
+            this.QuestionBox.TabIndex = 2;
+            // 
+            // NumberOfQuestion
+            // 
+            this.NumberOfQuestion.Location = new System.Drawing.Point(14, 463);
+            this.NumberOfQuestion.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.NumberOfQuestion.Name = "NumberOfQuestion";
+            this.NumberOfQuestion.Size = new System.Drawing.Size(115, 26);
+            this.NumberOfQuestion.TabIndex = 3;
+            this.NumberOfQuestion.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.NumberOfQuestion.ValueChanged += new System.EventHandler(this.NumberValueChanged);
+            // 
+            // TrueBox
+            // 
+            this.TrueBox.AutoSize = true;
+            this.TrueBox.Location = new System.Drawing.Point(164, 465);
+            this.TrueBox.Name = "TrueBox";
+            this.TrueBox.Size = new System.Drawing.Size(103, 24);
+            this.TrueBox.TabIndex = 4;
+            this.TrueBox.Text = "Правда?";
+            this.TrueBox.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(375, 417);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(321, 72);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Здесь могла бы быть ваша реклама...";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(708, 500);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.TrueBox);
+            this.Controls.Add(this.NumberOfQuestion);
+            this.Controls.Add(this.QuestionBox);
+            this.Controls.Add(this.SaveButton);
+            this.Controls.Add(this.DellButton);
+            this.Controls.Add(this.AddButton);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "MainForm";
+            this.Text = "Редактор файлов";
+            this.TransparencyKey = System.Drawing.Color.Gray;
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.NumberOfQuestion)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem файлToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem открытьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem сохранитьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem удалитьToolStripMenuItem;
+        private System.Windows.Forms.Button AddButton;
+        private System.Windows.Forms.Button DellButton;
+        private System.Windows.Forms.Button SaveButton;
+        private System.Windows.Forms.TextBox QuestionBox;
+        private System.Windows.Forms.NumericUpDown NumberOfQuestion;
+        private System.Windows.Forms.CheckBox TrueBox;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ dz_8/MainForm.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+using dz_8.Data;
+
+namespace dz_8
+{
+    public partial class MainForm : Form
+    {
+        private GameInfo _GameInfo = new GameInfo();
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void открытьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // создаём диалог для открытия файла
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Title = "Сохранение файла";
+            dialog.RestoreDirectory = true;
+            dialog.Filter = "Файлы xml (*.xml)|*.xml|Все файлы (*.*)|*.*";
+
+            var result = dialog.ShowDialog();
+            if (result != DialogResult.OK) return;
+            long length = new System.IO.FileInfo(dialog.FileName).Length;
+            if (length > 10000)
+            {
+                MessageBox.Show("Превышен лимит!", "Alert!",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+                return;
+            }
+            if (!File.Exists(dialog.FileName))
+            {
+                MessageBox.Show("Такого файла нет!", "Alert!",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+                return;
+            }
+
+            _GameInfo = GameInfo.Load(dialog.FileName);
+        }
+
+        private void сохранитьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // сохранение файла
+            SaveFileDialog dialog = new SaveFileDialog();
+            dialog.Title = "Сохранение файла";
+            dialog.RestoreDirectory = true;         // Восстанавливать предыдущий путь
+            dialog.Filter = "Файлы xml (*.xml)|*.xml|Все файлы (*.*)|*.*";
+            dialog.FileName = "Game.xml";
+
+            var result = dialog.ShowDialog();
+            if (result != DialogResult.OK) return;
+
+            var file_name = dialog.FileName;
+            _GameInfo.Save(file_name);
+        }
+
+        private void NumberValueChanged(object sender, EventArgs e)
+        {
+            //счетчик двигаем
+            NumericUpDown numeric_updown = (NumericUpDown)sender;
+            var question_number = (int)(numeric_updown.Value - 1);
+
+            if (question_number < 0 || question_number >= _GameInfo.Questions.Count)
+                return;
+
+            var question = _GameInfo.Questions[question_number];
+            QuestionBox.Text = question.Text;
+            TrueBox.Checked = question.IsTrue;
+        }
+
+        private void AddButton_Click(object sender, EventArgs e)
+        {
+            // добавляем новый вопрос
+            Questions new_question = new Questions();
+            _GameInfo.Questions.Add(new_question);
+            NumberOfQuestion.Value = _GameInfo.Questions.Count;
+            QuestionBox.Text = "";
+            TrueBox.Checked = true;
+        }
+
+        private void SaveButton_Click(object sender, EventArgs e)
+        {
+            // сохраняем вопрос
+            int question_number = (int)(NumberOfQuestion.Value - 1);
+            if (question_number < 0 || question_number >= _GameInfo.Questions.Count)
+                return;
+            Questions question = _GameInfo.Questions[question_number];
+            question.Text = QuestionBox.Text;
+            question.IsTrue = TrueBox.Checked;
+        }
+
+        private void DellButton_Click(object sender, EventArgs e)
+        {
+            // удаляем вопрос
+            DialogResult result = MessageBox.Show("Удаляем вопрос?", "Внимание!", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (result == DialogResult.Yes)
+            {
+                int question_number = (int)(NumberOfQuestion.Value - 1);
+                if (question_number < 0 || question_number >= _GameInfo.Questions.Count)
+                    return;
+                _GameInfo.Dell(question_number);
+                QuestionBox.Text = "";
+            }
+            if (result == DialogResult.No)
+            {
+                return;
+            }
+
+        }
+
+        private void СоздатьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            _GameInfo = new GameInfo();
+            UpdateInterface();
+        }
+
+        private void UpdateInterface()
+        {
+            // Надо обновить состояние интерфейса
+        }
+    }
+}
